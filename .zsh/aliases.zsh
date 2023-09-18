6fae9a5a692695f089d5b75896ca6c250db54991
Dennis Ollhoff <contact@nyxb.email>
2023-09-18 19:15:19 +0200

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,8 +1,16 @@
 # Alias
 # ---
 alias g="git"
-alias vs="code-insiders"
+alias vsi="code-insiders"
 alias oc="opencommit"
+alias cd2="cd ../../"
+alias n="nyxr"
+alias vs="code"
 
 # mac OS shortcuts
 # alias code="open -a 'Visual Studio Code'"
+
+# alias for terminal
+alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs code"
+alias ll="exa -l -g --icons --git"
+alias llt="exa -1 --icons --tree --git-ignore"
